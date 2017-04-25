1414e55ee33391635ef21efea53c5d40782a4ec7
Matthew Treinish <mtreinish@kortar.org>
2017-04-25 11:38:33 -0400

    Remove failed attempt to use ansible python API
    After consulting some people more well versed with ansible I decided
    attempting to use ansible's python API is a fools errand. Lets just
    stick with what works and use subprocess. This commit removes the failed
    attempts to use the python api.

--- handbrakecloud/runner.py
+++ handbrakecloud/runner.py
@@ -13,49 +13,9 @@
 import logging
 import subprocess
 
-from ansible.executor import playbook_executor
-from ansible import inventory as ansible_inv
-from ansible.parsing import dataloader
-from ansible import vars as variables
-from collections import namedtuple
-
 from handbrakecloud import exceptions
 
 LOG = logging.getLogger(__name__)
-
-
-OPTIONS = namedtuple('Options',
-                     ['listtags', 'listtasks', 'listhosts', 'syntax',
-                      'connection', 'module_path', 'forks', 'remote_user',
-                      'private_key_file', 'ssh_common_args', 'ssh_extra_args',
-                      'sftp_extra_args', 'scp_extra_args', 'become',
-                      'become_method', 'become_user', 'verbosity', 'check'])
-
-
-def run_playbook(playbook, extra_vars):
-    options = OPTIONS(listtags=False, listtasks=False, listhosts=False,
-                      syntax=False, connection='ssh', module_path=None,
-                      forks=100, remote_user='ubuntu', private_key_file=None,
-                      ssh_common_args=None, ssh_extra_args=None,
-                      sftp_extra_args=None, scp_extra_args=None, become=True,
-                      become_method=None, become_user='root', verbosity=3,
-                      check=False)
-
-    variable_manager = variables.VariableManager()
-    variable_manager.extra_vars = extra_vars
-    loader = dataloader.DataLoader()
-    inventory = ansible_inv.Inventory(loader=loader,
-                                      variable_manager=variable_manager,
-                                      host_list='localhost')
-    variable_manager.set_inventory(inventory)
-    executor = playbook_executor.PlaybookExecutor(
-        playbooks=[playbook],
-        inventory=inventory,
-        variable_manager=variable_manager,
-        loader=loader,
-        options=options,
-        passwords=None)
-    executor.run()
 
 
 def handle_error(stdout, stderr, return_code):
@@ -64,7 +24,6 @@ def handle_error(stdout, stderr, return_code):
         raise exceptions.InstanceCreateException()
 
 
-# Only using this until I can figure out the ansible python API
 def run_playbook_subprocess(playbook, extra_vars):
     extra_vars_string = ""
     for var in extra_vars:
